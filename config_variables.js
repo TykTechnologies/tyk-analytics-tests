430def5a490a6c4a9c48b0a44d4fb183cba7f3e9
Konrad Soltys <konrad@mbp-konrad.home>
2020-12-11 17:10:30 +0100

    change export to commonJs in config_variables.js file

--- config_variables.js
+++ config_variables.js
@@ -1,13 +1,21 @@
 require('dotenv').config();
 
-export const CLEAN_TEST = process.env.CLEAN_TEST || true;
-export const URL = process.env.URL || "http://localhost:3000/";
-export const DASHBOARD_API = URL + 'api/';
-export const DASHBOARD_ADMIN_API = URL + 'admin/';
-export const USER_EMAIL = process.env.USER_EMAIL;
-export const USER_SECRET = process.env.USER_SECRET;
-export const USER_PASSWORD = process.env.USER_PASSWORD || "test123";
-export const ORG_NAME = process.env.ORG_NAME || "Auto-test-org";
-export const ADMIN_SECRET = process.env.ADMIN_SECRET || "12345";
-export const ORG_API_PATH = "organisations/";
-export const USERS_API_PATH = "users/";
+const URL = process.env.URL || "http://localhost:3000/";
+
+module.exports = {
+    CLEAN_TEST: process.env.CLEAN_TEST || true,
+
+    URL: URL,
+    DASHBOARD_API: URL + 'api/',
+    DASHBOARD_ADMIN_API: URL + 'admin/',
+    
+    USER_EMAIL: process.env.USER_EMAIL,
+    USER_SECRET: process.env.USER_SECRET,
+    USER_PASSWORD: process.env.USER_PASSWORD || "test123",
+    
+    ORG_NAME: process.env.ORG_NAME || "Auto-test-org",
+    ADMIN_SECRET: process.env.ADMIN_SECRET || "12345",
+    
+    ORG_API_PATH: "organisations/",
+    USERS_API_PATH: "users/",
+};
