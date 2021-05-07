5eca96379d415d182d1eb9a63dbad4f0cf71f92b
Konrad Soltys <konrad@mbp-konrad.home>
2021-05-07 18:05:06 +0200

    disable oas_fields_validation test

--- test/specs/api_designer/oas_fields_validation_test.js
+++ test/specs/api_designer/oas_fields_validation_test.js
@@ -3,7 +3,7 @@ import { apis_page } from '../../../lib/pom/Apis_page';
 import { URL, LANDING_PAGE_PATH } from './../../../config_variables';
 import { expect } from 'chai';
 
-describe('Test mandatory fields on OAS API designer page', () => {
+xdescribe('Test mandatory fields on OAS API designer page', () => {
   const apiName = "oas-api-validation-test";
   let envDetails;
 
