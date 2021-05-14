4077bca1bbe014d9782fcd72ba946723566702ee
Maciej Wojciechowski <maciej@tyk.io>
2021-05-14 13:34:42 +0200

    - added possibility to upload cert over dash api - added test for SAML profile creation

--- test/specs/tib/add_tib_profiles_test.js
+++ test/specs/tib/add_tib_profiles_test.js
@@ -1,18 +1,19 @@
 import { login_page } from '../../../lib/pom/Login_page';
 import { main_page } from '../../../lib/pom/Main_page';
 import { tib_page } from '../../../lib/pom/Tib_page';
+import { Dashboard_connection } from '../../../lib/utils/api_connections/Dashboard_connection';
 
 let envDetails;
+const certFileLocation = './test/specs/tib/public_key.pem';
 const samlProfile = {
-  name: "user_name",
-  type: "user_last_name",
-  emailAdress: generateRandomEmail(),
-  password: "test123",
-  isUserAnAdmin: true
+  name: "samlProfile",
+  type: "SAML",
+  url: "https://cosiek.com"
 };
 
 describe('Users creation', () => {
-  
+  const dashboard_connection = new Dashboard_connection();
+
   before(() => {
     envDetails = setUpEnv();
     login_page.open();
@@ -25,9 +26,17 @@ describe('Users creation', () => {
   });
 
   it('User should be able to add SAML profile', () => {
-    
+    dashboard_connection.uploadCert(certFileLocation, envDetails.userSecret)
+    tib_page.PROFILE_NAME_INPUT.setValue(samlProfile.name);
+    tib_page.PROFILE_NEXT_BUTTON.click();
+    tib_page.selectProviderByName(samlProfile.type);
+    tib_page.PROVIDER_NEXT_BUTTON.click();
+    tib_page.CERTIFICATE_DROPDOWN.selectFirstOption();
+    tib_page.URL_INPUT.setValue(samlProfile.url);
+    tib_page.CREATE_PROFILE_BUTTON.click();
+    expect(tib_page.isProfileCreatedPopUpDisplayed()).to.be.true;
   });
-
+/*
   it('User should be able to add LDAP profile', () => {
     
   });
@@ -39,5 +48,5 @@ describe('Users creation', () => {
   it('User should be able to add Social profile', () => {
     
   });
-
+*/
 });
