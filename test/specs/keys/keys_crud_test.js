420f414b676f4525192d9206d9c383e3c4cdcc16
Bojan Kopunovic <bojan@tyk.io>
2021-10-11 12:44:59 +0200

    Added keys_crud_test

--- test/specs/keys/keys_crud_test.js
+++ test/specs/keys/keys_crud_test.js
@@ -4,16 +4,27 @@ import { keys_page } from '../../../lib/pom/Keys_page.js';
 import { Dashboard_connection } from '../../../lib/utils/api_connections/Dashboard_connection';
 import { newAPIdefinitionWithDefaults } from '../../../lib/utils/API_object_designer';
 
+const authTokenApi = {
+  "name": "authtoken",
+  "use_keyless": false,
+  "use_standard_auth": true
+};
 
-/*const policyDetails = {
-  apiName: 'test_policy',
-  policyName: 'policy_auto_test_1',
-  policyNameUpdate: 'policy_auto_test_1_update',
-  keyEpiryTime: "1 hour",
-  keyEpiryTimeUpdateValue: "6 hours"
-};*/
+const apiKeysDetails = {
+  apiName: 'keys_crud',
+  keyExpiryTime: "1 hour",
+  alias:'test',
+};
 
-describe('Create/update/delete keys', () => {
+const updatedKeyDetails={
+  keyExpiryTimeUpdateValue: "6 hours",
+  aliasUpdate:'updateAlias',
+  metadataKey:'test',
+  metadataValue:'123',
+};
+
+
+describe('Create/update/delete keys without policy', () => {
   const dashboard_connection = new Dashboard_connection();
   let envDetails;
 
@@ -23,22 +34,62 @@ describe('Create/update/delete keys', () => {
     login_page.login(envDetails.userEmail, envDetails.userPassword);
   });
 
-  /*it('Prerequisits: creating API definition via dashboard API', () => {
-    const apiDefinition = newAPIdefinitionWithDefaults({"name":policyDetails.apiName});
-    dashboard_connection.createAPI(apiDefinition, envDetails.userSecret);
-  });*/
+  it('Prerequisits: creating API definition via dashboard API', () => {
+      const body = newAPIdefinitionWithDefaults(authTokenApi);
+      dashboard_connection.createAPI(body, envDetails.userSecret);
+  });
 
   it('User should be able to create new Key', () => {
     main_page.openKeys();
     keys_page.ADD_KEY_BUTTON.click();
-
+    keys_page.CHOOSE_API_TOGGLE.click();
+    wdioExpect(keys_page.CHOOSE_API_TABLE).toHaveTextContaining(authTokenApi.name);
+    keys_page.CHOOSE_API_TABLE.clickCellWithText(authTokenApi.name);
+    keys_page.CONFIGURATIONS_TAB_BUTTON.click();
+    keys_page.ALIAS_INPUT_FIELD.click();
+    keys_page.ALIAS_INPUT_FIELD.setValue(apiKeysDetails.alias);
+    keys_page.KEY_EXPIRE_DROPDOWN.click();
+    keys_page.KEY_EXPIRE_DROPDOWN.selectOption(apiKeysDetails.keyExpiryTime);
+    keys_page.ENABLE_DETAILED_LOGGING_BUTTON.click();
+    keys_page.CREATE_KEY_BUTTON.click();
+    keys_page.OK_BUTTON.click();
   });
 
+ it ('User should be able to modify key',()=>{
+   const keyHashValue= keys_page.COPY_KEY_HASH_BUTTON.getAttribute('copy');
+   main_page.openKeys();
+   keys_page.KEY_SEARCH_FIELD.click();
+   keys_page.KEY_SEARCH_FIELD.setValue(keyHashValue);
+   keys_page.LOOKUP_KEY_BUTTON.click();
+   keys_page.CONFIGURATIONS_TAB_BUTTON.click();
+   wdioExpect(keys_page.KEY_HASH_VALUE).toHaveAttributeContaining('copy', keyHashValue);
+   wdioExpect(keys_page.UPDATE_BUTTON).toBeDisplayed();
+   wdioExpect(keys_page.UPDATE_WITHOUT_QUOTA_RESET_BUTTON).toBeDisplayed();
+   wdioExpect(keys_page.DELETE_BUTTON).toBeDisplayed();
+   wdioExpect(keys_page.ALIAS_INPUT_FIELD).toHaveValue(apiKeysDetails.alias);
+   keys_page.ALIAS_INPUT_FIELD.click();
+   keys_page.ALIAS_INPUT_FIELD.setValue(updatedKeyDetails.aliasUpdate);
+   keys_page.ENABLE_DETAILED_LOGGING_BUTTON.click();
+   keys_page.KEY_EXPIRE_DROPDOWN.click();
+   keys_page.KEY_EXPIRE_DROPDOWN.selectOption(updatedKeyDetails.keyExpiryTimeUpdateValue);
+   keys_page.METADATA_KEY_INPUT.setValue(updatedKeyDetails.metadataKey);
+   keys_page.METADATA_VALUE_INPUT.setValue(updatedKeyDetails.metadataValue);
+   keys_page.METADATA_ADD_BUTTON.click();
+   keys_page.UPDATE_BUTTON.click();
+   keys_page.CONFIRM_BUTTON.click();
+ });
 
+ it('Confirmation popup should be displayed', () => {
+  expect(keys_page.isKeyUpdatedPopUpDisplayed()).to.be.true;
+});
 
+ it('User should be able to delete key',()=>{
+  keys_page.DELETE_BUTTON.click();
+  keys_page.DELETE_KEY_CONFIRMATION_BUTTON.click();
+ });
 
-
-
-
+ it('Confirmation popup should be displayed', () => {
+  expect(keys_page.isKeyDeletedPopUpDisplayed()).to.be.true;
+});
 
 });
