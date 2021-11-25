5acdd2310e3a6d4a5caa562b5d3e090fe9fd2518
Maciej Wojciechowski <maciej@tyk.io>
2021-11-25 12:21:04 +0100

    added test for multiple Issuers

--- test/specs/api_designer/authentication_tests/oas_api_oidc_test.js
+++ test/specs/api_designer/authentication_tests/oas_api_oidc_test.js
@@ -8,7 +8,6 @@ var OasMapToPolicyTable_object = require('ui_test_automation/wrappers/OasMapToPo
 
 describe('Test OIDC Authentication in OAS API designer page', () => {
   let envDetails;
-  let firstApi = false;
   const dashboard_connection = new Dashboard_connection();
 
   before(() => {
@@ -38,11 +37,11 @@ describe('Test OIDC Authentication in OAS API designer page', () => {
     wdioExpect(issuerTargetUrlErrorMsg).toHaveText('Issuer target URL needs to have a valid URL value (eg. https://, http://)');
     wdioExpect(authKeyHeaderErrorMsg).toHaveText('Auth Key Header Name is required');
     apis_page.OAS_OIDC_ISSUER_ONE_TARGET_URL_INPUT.setValue('https://a.com');
-    apis_page.OAS_OIDC_ADD_CLIENT_BUTTON.click();
+    apis_page.OAS_OIDC_ADD_CLIENT_ONE_BUTTON.click();
     apis_page.OAS_SAVE_BUTTON.click();
     let mapClientToPolicyErrorMsg = $('//label[text()="Map Client to Policy"]//following::p[2]');
     wdioExpect(mapClientToPolicyErrorMsg).toHaveText("Clinet ID and Policy are required");
-    apis_page.OAS_OIDC_MAP_CLIENT_TO_POLICY_FIRST_TABLE.deleteMapping('');
+    apis_page.OAS_OIDC_MAP_CLIENT_TO_POLICY_ONE_TABLE.deleteMapping('');
     apis_page.OAS_OIDC_ADD_NEW_ISSUER_BUTTON.click();
     apis_page.OAS_OIDC_ISSUER_TWO_TARGET_URL_INPUT.setValue('https://a.com');
     browser.pause(1000);
@@ -57,13 +56,14 @@ describe('Test OIDC Authentication in OAS API designer page', () => {
     openInitPage(firstApi);
     apis_page.API_NAME_INPUT.setValue('oidc-test');
     apis_page.OAS_NEXT_BUTTON.click();
-    apis_page.OAS_ENABLE_AUTH_TOGGLE.click();
-    apis_page.OAS_AUTHENTICATION_DROPDOWN.selectOption("OpenID Connect");
-    apis_page.OAS_OIDC_ISSUER_ONE_TARGET_URL_INPUT.setValue('https://issuer-target.com');
-    apis_page.OAS_OIDC_AUTH_HEADER_INPUT.setValue('Authorization');
-    apis_page.SIDE_MENU_BASE_LINK.click();
     apis_page.OAS_GW_STATUS_DROPDOWN.selectOption("Active");
     apis_page.OAS_ACCESS_DROPDOWN.selectOption("External");
+    apis_page.SIDE_MENU_SERVER_LINK.click();
+    apis_page.OAS_ENABLE_AUTH_TOGGLE.click();
+    apis_page.OAS_AUTHENTICATION_DROPDOWN.selectOption("OpenID Connect");
+    apis_page.OAS_OIDC_ISSUER_ONE_TARGET_URL_INPUT.waitForClickable();
+    apis_page.OAS_OIDC_ISSUER_ONE_TARGET_URL_INPUT.setValue('https://issuer-target.com');
+    apis_page.OAS_OIDC_AUTH_HEADER_INPUT.setValue('Authorization');    
     apis_page.OAS_SAVE_BUTTON.click();
     expect(apis_page.isApiCreatedPopUpDisplayed()).to.be.true;
   });
@@ -106,19 +106,21 @@ describe('Test OIDC Authentication in OAS API designer page', () => {
   it('User can map Clients to Policies', () => {
     apis_page.EDIT_BUTTON.click();
     apis_page.SIDE_MENU_SERVER_LINK.click();
+    apis_page.OAS_OIDC_ENABLE_RL_PER_CLIENT_BOX.click();
     apis_page.OAS_OIDC_ISSUER_ONE_ACCORDTION.expand();
-    apis_page.OAS_OIDC_ADD_CLIENT_BUTTON.waitForExist();
+    apis_page.OAS_OIDC_ADD_CLIENT_ONE_BUTTON.waitForExist();
     apis_page.OAS_OIDC_ISSUER_ONE_TARGET_URL_INPUT.setValue('http://new-issuer.com');
-    apis_page.OAS_OIDC_ADD_CLIENT_BUTTON.click();
-    apis_page.OAS_OIDC_MAP_CLIENT_TO_POLICY_FIRST_TABLE.addNewMapping('http://client-a.com', 'policyA');
-    apis_page.OAS_OIDC_ADD_CLIENT_BUTTON.click();
-    apis_page.OAS_OIDC_MAP_CLIENT_TO_POLICY_FIRST_TABLE.addNewMapping('http://client-b.com', 'policyB');
+    apis_page.OAS_OIDC_ADD_CLIENT_ONE_BUTTON.click();
+    apis_page.OAS_OIDC_MAP_CLIENT_TO_POLICY_ONE_TABLE.addNewMapping('http://client-a.com', 'policyA');
+    apis_page.OAS_OIDC_ADD_CLIENT_ONE_BUTTON.click();
+    apis_page.OAS_OIDC_MAP_CLIENT_TO_POLICY_ONE_TABLE.addNewMapping('http://client-b.com', 'policyB');
     apis_page.OAS_SAVE_BUTTON.click();
     expect(apis_page.isApiUpdatedPopUpDisplayed()).to.be.true;
   });
 
   it('Clients to policies mapping is displayed after page reload', () => {
     browser.refresh();
+    wdioExpect(apis_page.OAS_OIDC_ENABLE_RL_PER_CLIENT_BOX).toBeChecked();
     apis_page.OAS_OIDC_ISSUER_ONE_ACCORDTION.expand();
     let clientToPolicyMappingSaved = new OasMapToPolicyTable_object('//label[text()="Map Client to Policy"]//following::ul[1]');
     let issuerTargetURLSaved = $('//span[text()="Issuer Target URL"]//following::div[2]');
@@ -130,15 +132,17 @@ describe('Test OIDC Authentication in OAS API designer page', () => {
   it('User can modify Clients to Policies mappings and Save API', () => {
     apis_page.EDIT_BUTTON.click();
     apis_page.SIDE_MENU_SERVER_LINK.click();
+    apis_page.OAS_OIDC_ENABLE_RL_PER_CLIENT_BOX.click();
     apis_page.OAS_OIDC_ISSUER_ONE_ACCORDTION.expand();
-    apis_page.OAS_OIDC_MAP_CLIENT_TO_POLICY_FIRST_TABLE.changeName('http://client-a.com', 'https://client-c.com');
-    apis_page.OAS_OIDC_MAP_CLIENT_TO_POLICY_FIRST_TABLE.changePolicy('http://client-b.com', 'policyC');
+    apis_page.OAS_OIDC_MAP_CLIENT_TO_POLICY_ONE_TABLE.changeName('http://client-a.com', 'https://client-c.com');
+    apis_page.OAS_OIDC_MAP_CLIENT_TO_POLICY_ONE_TABLE.changePolicy('http://client-b.com', 'policyC');
     apis_page.OAS_SAVE_BUTTON.click();
     expect(apis_page.isApiUpdatedPopUpDisplayed()).to.be.true;
   });
 
   it('Updated Clients to policies mapping is displayed after page reload', () => {
     browser.refresh();
+    wdioExpect(apis_page.OAS_OIDC_ENABLE_RL_PER_CLIENT_BOX).not.toBeChecked();
     apis_page.OAS_OIDC_ISSUER_ONE_ACCORDTION.expand();
     let clientToPolicyMappingSaved = new OasMapToPolicyTable_object('//label[text()="Map Client to Policy"]//following::ul[1]');
     expect(clientToPolicyMappingSaved.getPolicyValueForCell('https://client-c.com')).to.equal('policyA');
@@ -148,7 +152,7 @@ describe('Test OIDC Authentication in OAS API designer page', () => {
   it('User can remove Client mapping and Save API', () => {
     apis_page.EDIT_BUTTON.click();
     apis_page.SIDE_MENU_SERVER_LINK.click();
-    apis_page.OAS_OIDC_MAP_CLIENT_TO_POLICY_FIRST_TABLE.deleteMapping('https://client-c.com');
+    apis_page.OAS_OIDC_MAP_CLIENT_TO_POLICY_ONE_TABLE.deleteMapping('https://client-c.com');
     apis_page.OAS_SAVE_BUTTON.click();
     expect(apis_page.isApiUpdatedPopUpDisplayed()).to.be.true;
   });
@@ -170,6 +174,10 @@ describe('Test OIDC Authentication in OAS API designer page', () => {
     apis_page.OAS_OIDC_MAP_CLAIM_TO_POLICY_FIRST_TABLE.addNewMapping('claimA', 'policyA');
     apis_page.OAS_OIDC_ADD_CLAIM_BUTTON.click();
     apis_page.OAS_OIDC_MAP_CLAIM_TO_POLICY_FIRST_TABLE.addNewMapping('claimB', 'policyB');
+    apis_page.OAS_OIDC_AUTH_HEADER_INPUT.setValue('Authorization-new');
+    apis_page.OAS_STRIP_AUTHORIZATION_DATA_BOX.click();
+    apis_page.OAS_OIDC_ALLOW_QUERY_PARAM_BOX.click();
+    apis_page.OAS_OIDC_QUERY_PARAM_INPUT.setValue('query-param');
     apis_page.OAS_SAVE_BUTTON.click();
     expect(apis_page.isApiUpdatedPopUpDisplayed()).to.be.true;
   });
@@ -179,9 +187,15 @@ describe('Test OIDC Authentication in OAS API designer page', () => {
     wdioExpect(apis_page.OAS_OIDC_ENABLE_SCOPE_CLAIMS_BOX).toBeChecked();
     let scopeNameSaved = $('//span[text()="Scope Name"]//following::div[2]');
     let claimToPolicyMappingSaved = new OasMapToPolicyTable_object('//label[text()="Map Claim to Policy"]//following::ul[1]');
+    let authKeyHeaderNameSaved = $('//label[text()="Auth Key Header Name"]//following::div[1]');
+    let queryParamNameSaved = $('//label[text()="Query parameter Name"]//following::div[1]');
     wdioExpect(scopeNameSaved).toHaveText('my-scope');
     expect(claimToPolicyMappingSaved.getPolicyValueForCell('claimA')).to.equal('policyA');
     expect(claimToPolicyMappingSaved.getPolicyValueForCell('claimB')).to.equal('policyB');
+    wdioExpect(apis_page.OAS_OIDC_ALLOW_QUERY_PARAM_BOX).toBeChecked();
+    wdioExpect(apis_page.OAS_STRIP_AUTHORIZATION_DATA_BOX).toBeChecked();
+    wdioExpect(authKeyHeaderNameSaved).toHaveText('Authorization-new');
+    wdioExpect(queryParamNameSaved).toHaveText('query-param');
   });
 
   it('User can modify Scope Claims and Save API', () => {
@@ -190,6 +204,10 @@ describe('Test OIDC Authentication in OAS API designer page', () => {
     apis_page.OAS_OIDC_SCOPE_NAME_INPUT.setValue('my-scope-new');
     apis_page.OAS_OIDC_MAP_CLAIM_TO_POLICY_FIRST_TABLE.changeName('claimA', 'claimD');
     apis_page.OAS_OIDC_MAP_CLAIM_TO_POLICY_FIRST_TABLE.changePolicy('claimB', 'policyC');
+    apis_page.OAS_OIDC_QUERY_PARAM_INPUT.setValue('query-param-new');
+    apis_page.OAS_OIDC_USE_COOKIE_BOX.click();
+    apis_page.OAS_OIDC_COOKIE_VALUE_INPUT.setValue('my-cookie');
+    apis_page.OAS_STRIP_AUTHORIZATION_DATA_BOX.click();
     apis_page.OAS_SAVE_BUTTON.click();
     expect(apis_page.isApiUpdatedPopUpDisplayed()).to.be.true;
   });
@@ -199,15 +217,23 @@ describe('Test OIDC Authentication in OAS API designer page', () => {
     wdioExpect(apis_page.OAS_OIDC_ENABLE_SCOPE_CLAIMS_BOX).toBeChecked();
     let scopeNameSaved = $('//span[text()="Scope Name"]//following::div[2]');
     let claimToPolicyMappingSaved = new OasMapToPolicyTable_object('//label[text()="Map Claim to Policy"]//following::ul[1]');
+    let queryParamNameSaved = $('//label[text()="Query parameter Name"]//following::div[1]');
+    let cookieNameSaved = $('//label[text()="Cookie Name"]//following::div[1]');
     wdioExpect(scopeNameSaved).toHaveText('my-scope-new');
     expect(claimToPolicyMappingSaved.getPolicyValueForCell('claimD')).to.equal('policyA');
     expect(claimToPolicyMappingSaved.getPolicyValueForCell('claimB')).to.equal('policyC');
+    wdioExpect(apis_page.OAS_OIDC_ALLOW_QUERY_PARAM_BOX).toBeChecked();
+    wdioExpect(apis_page.OAS_STRIP_AUTHORIZATION_DATA_BOX).not.toBeChecked();
+    wdioExpect(queryParamNameSaved).toHaveText('query-param-new');
+    wdioExpect(cookieNameSaved).toHaveText('my-cookie');
   });
 
   it('User can remove Scope Claim and Save API', () => {
     apis_page.EDIT_BUTTON.click();
     apis_page.SIDE_MENU_SERVER_LINK.click();
     apis_page.OAS_OIDC_MAP_CLAIM_TO_POLICY_FIRST_TABLE.deleteMapping('claimB');
+    apis_page.OAS_OIDC_USE_COOKIE_BOX.click();
+    apis_page.OAS_OIDC_ALLOW_QUERY_PARAM_BOX.click();
     apis_page.OAS_SAVE_BUTTON.click();
     expect(apis_page.isApiUpdatedPopUpDisplayed()).to.be.true;
   });
@@ -218,6 +244,8 @@ describe('Test OIDC Authentication in OAS API designer page', () => {
     let claimToPolicyMappingSaved = new OasMapToPolicyTable_object('//label[text()="Map Claim to Policy"]//following::ul[1]');
     expect(claimToPolicyMappingSaved.checkIfRowExists('claimB')).to.be.false;
     expect(claimToPolicyMappingSaved.getPolicyValueForCell('claimD')).to.equal('policyA');
+    wdioExpect(apis_page.OAS_OIDC_ALLOW_QUERY_PARAM_BOX).not.toBeChecked();
+    wdioExpect(apis_page.OAS_OIDC_USE_COOKIE_BOX).not.toBeChecked();
   });
 
   xit('User can disable Scope Claims and Save API', () => {
@@ -233,6 +261,53 @@ describe('Test OIDC Authentication in OAS API designer page', () => {
     wdioExpect(apis_page.OAS_OIDC_ENABLE_SCOPE_CLAIMS_BOX).not.toBeChecked();
     let scopeNameSaved = $('//span[text()="Scope Name"]//following::div[2]');
     wdioExpect(scopeNameSaved).not.toBeExisting();
+  });
+
+  it('User can add more issuers', () => {
+    apis_page.EDIT_BUTTON.click();
+    apis_page.SIDE_MENU_SERVER_LINK.click();
+    apis_page.OAS_OIDC_ADD_NEW_ISSUER_BUTTON.click();
+    apis_page.OAS_OIDC_ISSUER_ONE_ACCORDTION.expand();
+    apis_page.OAS_OIDC_ISSUER_TWO_ACCORDTION.expand();
+    apis_page.OAS_OIDC_ISSUER_TWO_TARGET_URL_INPUT.setValue('http://second-issuer.com');
+    apis_page.OAS_OIDC_ADD_CLIENT_TWO_BUTTON.click();
+    apis_page.OAS_OIDC_MAP_CLIENT_TO_POLICY_TWO_TABLE.addNewMapping('http://second-client.com', 'policyA');
+    apis_page.OAS_SAVE_BUTTON.click();
+    expect(apis_page.isApiUpdatedPopUpDisplayed()).to.be.true;
+  });
+
+  it('Clients to policies mapping is displayed after page reload', () => {
+    browser.refresh();
+    apis_page.OAS_OIDC_ISSUER_ONE_ACCORDTION.expand();
+    apis_page.OAS_OIDC_ISSUER_TWO_ACCORDTION.expand();
+    let clientToPolicyMappingOneSaved = new OasMapToPolicyTable_object('//h3[text()="Issuer one"]//following::ul[1]');
+    let clientToPolicyMappingTwoSaved = new OasMapToPolicyTable_object('//h3[text()="Issuer two"]//following::ul[1]');
+    let issuerOneTargetURLSaved = $('//h3[text()="Issuer one"]//following::span[text()="Issuer Target URL"]//following::div[2]');
+    let issuerTwoTargetURLSaved = $('//h3[text()="Issuer two"]//following::span[text()="Issuer Target URL"]//following::div[2]');
+    wdioExpect(issuerOneTargetURLSaved).toHaveText('http://second-issuer.com');
+    wdioExpect(issuerTwoTargetURLSaved).toHaveText('http://new-issuer.com');
+    expect(clientToPolicyMappingOneSaved.getPolicyValueForCell('http://second-client.com')).to.equal('policyA');
+    expect(clientToPolicyMappingTwoSaved.getPolicyValueForCell('http://client-b.com')).to.equal('policyC');
+  });
+
+  it('User can remove issuer', () => {
+    apis_page.EDIT_BUTTON.click();
+    apis_page.SIDE_MENU_SERVER_LINK.click();
+    apis_page.OAS_OIDC_ISSUER_ONE_ACCORDTION.expand();
+    apis_page.OAS_OIDC_ISSUER_TWO_ACCORDTION.expand();
+    let deleteIssuerButton = $('//h3[text()="Issuer two"]//following::span[text()="Delete Issuer"]');
+    deleteIssuerButton.click();
+    apis_page.OAS_SAVE_BUTTON.click();
+    expect(apis_page.isApiUpdatedPopUpDisplayed()).to.be.true;
+  });
+
+  it('Clients to policies mapping is displayed after page reload', () => {
+    browser.refresh();
+    apis_page.OAS_OIDC_ISSUER_ONE_ACCORDTION.expand();
+    let clientToPolicyMappingSaved = new OasMapToPolicyTable_object('//label[text()="Map Client to Policy"]//following::ul[1]');
+    let issuerTargetURLSaved = $('//span[text()="Issuer Target URL"]//following::div[2]');
+    wdioExpect(issuerTargetURLSaved).toHaveText('http://second-issuer.com');
+    expect(clientToPolicyMappingSaved.getPolicyValueForCell('http://second-client.com')).to.equal('policyA');
   });
 
   function openInitPage(firstApi) {
