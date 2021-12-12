be087e86dde8bb70def87e05ea81ee1f55e6f07a
Maciej Wojciechowski <maciej@tyk.io>
2021-12-12 18:10:00 +0100

    1. updated test description 2. modified steps to use real navigation instead of browser.url

--- test/specs/login_existing_url_test.js
+++ test/specs/login_existing_url_test.js
@@ -1,17 +1,20 @@
 import { login_page } from '../../lib/pom/Login_page';
+import { main_page } from '../../lib/pom/Main_page';
+import { users_page } from '../../lib/pom/Users_page';
 import { URL } from './../../config_variables';
 
-describe('Test CORS settings on OAS API designer page', () => {
+describe('Test navigation scenario when user sesion has expired', () => {
  
-  it('When sesion is expired proper URL is opened after logging in', () => {
+  it('Proper URL is opened after logging in', () => {
     const expectedEndpoint = 'users/add';
     const authCookie = 'authorisation';
     const pageTitle = $('h1=Add / Edit User');
     let envDetails = setUpEnv();
     login_page.open();
     login_page.login(envDetails.userEmail, envDetails.userPassword);
+    main_page.USERS_NAVIAGTION_BUTTON.click();
     browser.deleteCookies(authCookie);
-    browser.navigateTo(URL + expectedEndpoint);
+    users_page.ADD_USER_BUTTON.click();
     wdioExpect(login_page.USERNAME_INPUT).toBeDisplayed();
     login_page.login(envDetails.userEmail, envDetails.userPassword);
     wdioExpect(browser).toHaveUrl(URL + expectedEndpoint);
