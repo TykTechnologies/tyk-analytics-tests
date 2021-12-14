d2c2def5a6e3c5e969466429e0b68ee9257299db
konrad <konrad@tyk.io>
2021-12-14 14:50:08 +0100

    fix for federation

--- wdio.conf.js
+++ wdio.conf.js
@@ -20,7 +20,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './test/specs/**/*.js'
+        './test/specs/graphql/crud_federation_test.js'
     ],
     // Patterns to exclude.
     exclude: [
