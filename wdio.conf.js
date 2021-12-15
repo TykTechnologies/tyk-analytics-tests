54c229a4671258d2c0cef5ee250b99ee371e4df6
konrad <konrad@tyk.io>
2021-12-15 10:32:08 +0100

    change scope of tests

--- wdio.conf.js
+++ wdio.conf.js
@@ -20,7 +20,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './test/specs/graphql/crud_federation_test.js'
+        './test/specs/**/*.js'
     ],
     // Patterns to exclude.
     exclude: [
