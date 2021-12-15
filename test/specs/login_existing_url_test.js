ffac50f5a967d8bebf602f5e4fd3f48874ebaece
Maciej Wojciechowski <maciej@tyk.io>
2021-12-15 10:37:53 +0100

    modified logic, functionality is supposed to work with manually entered URL (and not navigation)

--- test/specs/login_existing_url_test.js
+++ test/specs/login_existing_url_test.js
@@ -1,21 +1,13 @@
 import { login_page } from '../../lib/pom/Login_page';
-import { main_page } from '../../lib/pom/Main_page';
-import { users_page } from '../../lib/pom/Users_page';
 import { URL } from './../../config_variables';
 
-describe('Test navigation scenario when user sesion has expired', () => {
+describe('Test opening nested page when user is not logged in', () => {
  
   it('Proper URL is opened after logging in', () => {
     const expectedEndpoint = 'users/add';
-    const authCookie = 'authorisation';
     const pageTitle = $('h1=Add / Edit User');
     let envDetails = setUpEnv();
-    login_page.open();
-    login_page.login(envDetails.userEmail, envDetails.userPassword);
-    main_page.USERS_NAVIAGTION_BUTTON.click();
-    browser.deleteCookies(authCookie);
-    users_page.ADD_USER_BUTTON.click();
-    wdioExpect(login_page.USERNAME_INPUT).toBeDisplayed();
+    browser.navigateTo(URL + expectedEndpoint);
     login_page.login(envDetails.userEmail, envDetails.userPassword);
     wdioExpect(browser).toHaveUrl(URL + expectedEndpoint);
     wdioExpect(pageTitle).toBeDisplayed();
