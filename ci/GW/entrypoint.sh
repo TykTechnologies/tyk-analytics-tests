47edfa315913776fa96db9f2114c58e85308b270
konrad <konrad@tyk.io>
2022-03-07 10:05:49 +0100

    update CI env

---
+++ ci/GW/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+tyk_args=(--debug --httpprofile)
+
+tyk_args+=("--conf=/opt/tyk-gateway/tyk.conf")
+
+echo "Installing Tyk..."
+cd /opt/tyk-gateway
+ls
+go install
+
+echo "Running Tyk with following arguments:"
+echo "${tyk_args[@]}"
+
+"${GOPATH:-}/bin/tyk" "${tyk_args[@]}"
