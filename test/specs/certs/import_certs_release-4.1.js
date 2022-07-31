24e0e66eca120b779624dc3cdaf9285b3f03fa15
Mladen <mladen@tyk.io>
2022-07-31 13:53:42 +0200

    Test WIP and added cert file

--- test/specs/certs/import_certs_release-4.1.js
+++ test/specs/certs/import_certs_release-4.1.js
@@ -1,8 +1,11 @@
 
 import { login_page } from '../../../lib/pom/Login_page';
 import { main_page } from '../../../lib/pom/Main_Page';
+import {certs_page} from '../../../lib/pom/Certificates_page';
+import { path } from 'chromedriver';
+import Input_object from 'ui_test_automation/wrappers/Input_object';
 
-describe('HIGH LEVEL DESCRIPTION OF FEATURE', () => {
+describe('Import certificates', () => {
 
     before(() => {
         const envDetails = setUpEnv();
@@ -10,11 +13,22 @@ describe('HIGH LEVEL DESCRIPTION OF FEATURE', () => {
         login_page.login(envDetails.userEmail, envDetails.userPassword);
     });  
 
-  it('User should be able to.....', () => {
+  it('User should be able to import certificate', () => {
+    const filePath = path.join(__dirname, '../certs/cert.pem');
+    const remoteFilePath = browser.uploadFile(filePath);
+    const chooseFile = certs_page.CHOOSE_FILE_BUTTON;
+    main_page.openCertificates();
+    certs_page.ADD_CERTIFICATE_BUTTON.click();
+    chooseFile.setValue(remoteFilePath);
+    certs_page.UPLOAD_BUTTON();
+    browser.pause(5000);
+
+
+
 
   });
 
-  it('User should be able to......', () => {
+  it('User should be able to remove certificate', () => {
 
   });
 });
