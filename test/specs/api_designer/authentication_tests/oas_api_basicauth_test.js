f5a5056c03f3b985570fac687a7512332642664b
Maciej Wojciechowski <maciej@tyk.io>
2022-09-16 09:21:00 +0200

    fix multiAuth OAS test

--- test/specs/api_designer/authentication_tests/oas_api_basicauth_test.js
+++ test/specs/api_designer/authentication_tests/oas_api_basicauth_test.js
@@ -3,7 +3,7 @@ import { apis_page } from '../../../../lib/pom/Apis_page';
 import { main_page } from '../../../../lib/pom/Main_page';
 import { expect } from 'chai';
 
-xdescribe('Test Basic Authentication in OAS API designer page', () => {
+describe('Test Basic Authentication in OAS API designer page', () => {
   let envDetails;
 
   before(() => {
