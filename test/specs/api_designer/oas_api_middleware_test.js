6263f747230ef46b09083be8f4f1252122e1ff1d
Maciej Wojciechowski <maciej@tyk.io>
2022-12-22 12:49:27 +0100

    added Ignore Middleware tests

--- test/specs/api_designer/oas_api_middleware_test.js
+++ test/specs/api_designer/oas_api_middleware_test.js
@@ -17,7 +17,7 @@ describe('Test Middleware on OAS Endpoints designer page', () => {
     main_page.openAPIs();
     apis_page.DESIGN_API_BOX.click();
     apis_page.API_TYPE_OAS_BUTTON.click();
-    apis_page.API_NAME_INPUT.setValue('ignore-plugin');
+    apis_page.API_NAME_INPUT.setValue('middleware-test');
     apis_page.OAS_NEXT_BUTTON.click();
     apis_page.OAS_GW_STATUS_DROPDOWN.selectOption("Active");
     apis_page.OAS_ACCESS_DROPDOWN.selectOption("External");
@@ -41,6 +41,42 @@ describe('Test Middleware on OAS Endpoints designer page', () => {
     endpoints_page.changeMiddlewareStatusByName("Ignore Authentication");
     apis_page.OAS_SAVE_BUTTON.click();
     expect(apis_page.isApiUpdatedPopUpDisplayed()).to.be.true;
+  });
+
+  it('Ignore middelware is disabled after page reload', () => {
+    browser.refresh();
+    endpoints_page.OAS_ENDPOINTS_BUTTON.click();
+    expect(endpoints_page.verifyMiddlewareDisabledByName("Ignore Authentication")).to.be.true;
+  });
+
+  it('User can enable Ignore middelware and save API', () => {
+    browser.refresh();
+    endpoints_page.OAS_ENDPOINTS_BUTTON.click();
+    apis_page.EDIT_BUTTON.click();
+    endpoints_page.changeMiddlewareStatusByName("Ignore Authentication");
+    apis_page.OAS_SAVE_BUTTON.click();
+    expect(apis_page.isApiUpdatedPopUpDisplayed()).to.be.true;
+  });
+
+  it('Ignore middelware is enabled after page reload', () => {
+    browser.refresh();
+    endpoints_page.OAS_ENDPOINTS_BUTTON.click();
+    expect(endpoints_page.verifyMiddlewareDisabledByName("Ignore Authentication")).to.be.false;
+  });
+
+  it('User can remove Ignore middelware and save API', () => {
+    browser.refresh();
+    endpoints_page.OAS_ENDPOINTS_BUTTON.click();
+    apis_page.EDIT_BUTTON.click();
+    endpoints_page.removeMiddlewareByName("Ignore Authentication");
+    apis_page.OAS_SAVE_BUTTON.click();
+    expect(apis_page.isApiUpdatedPopUpDisplayed()).to.be.true;
+  });
+
+  it('Ignore middelware is removed after page reload', () => {
+    browser.refresh();
+    endpoints_page.OAS_ENDPOINTS_BUTTON.click();
+    expect(endpoints_page.verifyMiddlewareExistsByName("Ignore Authentication")).to.be.false;
   });
 
 });
