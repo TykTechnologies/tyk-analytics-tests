c3c271d39c03b46e0e11ae9a6b8cc2077b55dd98
konrad <konrad@tyk.io>
2023-09-05 11:15:29 +0200

    increase number of workers in CI

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 2 : 3,
+  workers: process.env.CI ? 3 : 3,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI ? [['github'], ['html']] : [['html'], ['json', { outputFile: 'playwright-report/summary.json' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
