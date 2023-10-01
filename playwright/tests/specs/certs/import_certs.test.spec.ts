6ab23203c34ba87b83011dc5ce1dba55c5b038a9
Mladen Kolavcic <kolavcic@gmail.com>
2023-10-01 23:06:23 +0200

    Updated login function

--- playwright/tests/specs/certs/import_certs.test.spec.ts
+++ playwright/tests/specs/certs/import_certs.test.spec.ts
@@ -1,46 +1,52 @@
-
-import { test, assert } from '@lib/utils/fixtures';
+import { test, assert } from '@fixtures';
+import { Login_page } from '@pom/Login_page';
+import { setUpEnv } from '@lib/utils/prerequisites';
 
 const path = require('path');
 
 
-test('Import certificates', async ({ main_page, certs_page, page }) => {
+test('Import certificates', async ({ main_page, certificates_page, page }) => {
   const FileRelativePath = "./full-cert.pem";
 
-  before(() => {
-    const envDetails = setUpEnv();
-    login_page.open();
-    login_page.login(envDetails.userEmail, envDetails.userPassword);
-  });  
+  const userDetails = await setUpEnv();
+  const login_page = new Login_page(page);
+  await login_page.open();
+  await login_page.login(userDetails.userEmail, userDetails.userPassword);
+  console.log(`>>> User is logged in! User Email: ${userDetails.userEmail}`);
+  use(userDetails); 
 
   await test.step('User should be able to import certificate', async () => {
     await main_page.openCertificates();
-    certs_page.ADD_CERTIFICATE_BUTTON.click();
-    uploadZipFile();
-    certs_page.UPLOAD_BUTTON.click();
-    wdioExpect(certs_page.ADDED_CERT_MESAGE_ALERT).toBeDisplayed();
+    await certificates_page.ADD_CERTIFICATE_BUTTON.click();
+    await uploadZipFile();
+    await certificates_page.UPLOAD_BUTTON.click();
+    await assert(certificates_page.ADDED_CERT_MESAGE_ALERT).toBeVisible();
   });
 
-  it('User should not be able to import duplicated certificate', () => {
-    certs_page.ADD_NEW_CERTIFICATE.click();
-    uploadZipFile();
-    certs_page.UPLOAD_BUTTON.click();
-    wdioExpect(certs_page.UPLOAD_ERROR_MESSAGE).toBeDisplayed();
+  await test.step('User should not be able to import duplicated certificate', async() => {
+    await certificates_page.ADD_NEW_CERTIFICATE.click();
+    await uploadZipFile();
+    await certificates_page.UPLOAD_BUTTON.click();
+    await assert(certificates_page.UPLOAD_ERROR_MESSAGE).toBeVisible();
   });
 
-  it('User should be able to remove certificate', () => {
-    certs_page.BACK_BUTTON.click();
-    certs_page.ACTIONS_BUTTON.click();
-    certs_page.ACTIONS_VIEW_BUTTON.click();
-    certs_page.DELETE_BUTTON.click();
-    certs_page.POPUP_DELETE.click();
-    wdioExpect(certs_page.REMOVED_CERT_MESSAGE_ALERT).toBeDisplayed();
+  await test.step('User should be able to remove certificate', async() => {
+    await certificates_page.BACK_BUTTON.click();
+    await certificates_page.ACTIONS_BUTTON.click();
+    await certificates_page.ACTIONS_VIEW_BUTTON.click();
+    await certificates_page.DELETE_BUTTON.click();
+    await certificates_page.POPUP_DELETE.click();
+    await assert(certificates_page.REMOVED_CERT_MESSAGE_ALERT).toBeVisible();
   });
 
   const uploadZipFile = () => {
     const filePath = path.join(__dirname, FileRelativePath);
     console.log('>>> Uploading certificate from path: ${filePath}');
     const remoteFilePath = browser.uploadFile(filePath);
-    certs_page.CHOOSE_FILE_BUTTON.setValue(remoteFilePath);
+    certificates_page.CHOOSE_FILE_BUTTON.setValue(remoteFilePath);
   };
 });
+
+function use(userDetails: any) {
+    throw new Error('Function not implemented.');
+}
