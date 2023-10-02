b542171bf6c3deffc8bcb45ed6a145c0b92071ff
Mladen Kolavcic <kolavcic@gmail.com>
2023-10-02 11:45:57 +0200

    Updated upload func

--- playwright/tests/specs/certs/import_certs.test.spec.ts
+++ playwright/tests/specs/certs/import_certs.test.spec.ts
@@ -6,7 +6,7 @@ const path = require('path');
 
 
 test('Import certificates', async ({ main_page, certificates_page, page }) => {
-  // const FileRelativePath = "./full_cert.pem"
+  const FileRelativePath = "./full_cert.pem"
 
   const userDetails = await setUpEnv();
   const login_page = new Login_page(page);
@@ -16,29 +16,36 @@ test('Import certificates', async ({ main_page, certificates_page, page }) => {
 
 
   const uploadZipFile = async (browser: any) => {
-    const filePath = path.join(__dirname, "./full_cert.pem");
-    console.log(`>>> Uploading certificate from path: ${filePath}`);
+  //   const filePath = path.join(__dirname, "./full_cert.pem");
+  //   console.log(`>>> Uploading certificate from path: ${filePath}`);
 
     // const remoteFilePath = browser.uploadFile(filePath);
-    await page.getByLabel('Upload certificate file').setInputFiles("./full_cert.pem");
+    // await page.getByLabel('Upload certificate file').setInputFiles("./full_cert.pem");
+
+    // Start waiting for file chooser before clicking. Note no await.
+    const fileChooserPromise = page.waitForEvent('filechooser');
+    await certificates_page.UPLOAD_BUTTON.click();
+    const fileChooser = await fileChooserPromise;
+    await fileChooser.setFiles(path.join(__dirname, FileRelativePath));
+
   };
-  
+
   await test.step('User should be able to import certificate', async () => {
     await main_page.openCertificates();
     await certificates_page.ADD_CERTIFICATE_BUTTON.click();
     await uploadZipFile(page); // pass the page argument to the function
-    await certificates_page.UPLOAD_BUTTON.click();
+    // await certificates_page.UPLOAD_BUTTON.click();
     await assert(certificates_page.ADDED_CERT_MESAGE_ALERT).toBeVisible();
   });
 
-  await test.step('User should not be able to import duplicated certificate', async() => {
+  await test.step('User should not be able to import duplicated certificate', async () => {
     await certificates_page.ADD_NEW_CERTIFICATE.click();
     await uploadZipFile(page); // pass the page argument to the function
-    await certificates_page.UPLOAD_BUTTON.click();
+    // await certificates_page.UPLOAD_BUTTON.click();
     await assert(certificates_page.UPLOAD_ERROR_MESSAGE).toBeVisible();
   });
 
-  await test.step('User should be able to remove certificate', async() => {
+  await test.step('User should be able to remove certificate', async () => {
     await certificates_page.BACK_BUTTON.click();
     await certificates_page.ACTIONS_BUTTON.click();
     await certificates_page.ACTIONS_VIEW_BUTTON.click();
